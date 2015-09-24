da82185aae3114b9a249139deb30b473fa3590fa
Denis Shagimuratov <poluxster@gmail.com>
2015-09-24 14:32:11 +0500

    Beta 1.

--- sql/mysql/install.mysql.utf8.sql
+++ sql/mysql/install.mysql.utf8.sql
@@ -1,1 +1,5 @@
-1;
+CREATE TABLE `#__sfw_networks` (
+  `network` int(11) unsigned NOT NULL,
+  `mask` int(11) unsigned NOT NULL,
+  KEY `network` (`network`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
