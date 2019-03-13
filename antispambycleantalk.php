0feb661880dc9196d2cde68d966b65c825994f1b
davydov <artem@ip-192-168-0-101.is74.loc>
2019-03-14 00:39:13 +0500

    update  version

--- antispambycleantalk.php
+++ antispambycleantalk.php
@@ -3,7 +3,7 @@
 /**
  * CleanTalk joomla plugin
  *
- * @version 6.0
+ * @version 6.1
  * @package Cleantalk
  * @subpackage Joomla
  * @author CleanTalk (welcome@cleantalk.org) 
@@ -27,7 +27,7 @@ class plgSystemAntispambycleantalk extends JPlugin
     /**
      * Plugin version string for server
      */
-    const ENGINE = 'joomla3-60';
+    const ENGINE = 'joomla3-61';
 
     /*
      * Flag marked JComments form initilization. 
